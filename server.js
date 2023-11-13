d59f48a65ab0bf0b1f49459d19344427501907ce
Camsca <camila_vass@hotmail.com>
2023-11-13 12:05:28 -0800

    setting server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');   
+const express = require('express');
+
+
+
+const sequelize = require('./config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});

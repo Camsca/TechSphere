00f528dd94d76e164544a3e10809f37408a5d0b8
Camsca <camila_vass@hotmail.com>
2023-11-13 16:58:07 -0800

    models index

--- models/index.js
+++ models/index.js
@@ -1,11 +1,31 @@
 const User = require('./user');
+const BlogPost = require('./BlogPost');
+const Comment = require('./comment');
 
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+ 
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 
+});
+BlogPost.belongsTo(User, {  
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 
-
+});
 
 
 module.exports = { 
     User,
+    BlogPost,
+    Comment
  };
